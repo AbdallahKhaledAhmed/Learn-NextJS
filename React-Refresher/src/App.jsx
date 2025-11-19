13ad78e86e7171e80d0fb87f3e223adc4bce3222
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-11-19 10:48:57 +0200

    components

--- React-Refresher/src/App.jsx
+++ React-Refresher/src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Card } from "./components/Card"
+
+function User ({name,age,gender}) {
+return (
+  <div>
+    <h1>name:{name}</h1>
+    <p>age:{age}</p>
+    <p>gender:{gender}</p>
+  </div>
+)
+}
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Card>
+     <User name="John" age="30" gender="Male"/>
+    </Card>
+     <User name="Jane" age="25" gender="Female"/>
+     <User name="Jack" age="40" gender="Male"/>
     </>
   )
 }
---
+++ React-Refresher/src/components/Card.jsx
@@ -0,0 +1,3 @@
+export function Card({children}) {
+    return <div style={{border: "solid 1px", padding:"5px 50px"}}>{children}</div>
+}
