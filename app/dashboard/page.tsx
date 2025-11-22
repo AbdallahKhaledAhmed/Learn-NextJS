539bf7e4c95c5b259304e6e0767e8e0b6ae0e1b9
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-11-22 22:02:16 +0200

    Tasks Page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,14 +2,17 @@ import Link from "next/link";
 
 const Dashboard = () => {
   return (
-    <div className="rounded w-[60%] mx-auto mt-15 shadow-lg text-gray-600">
+    <div className="rounded w-[90%] md:w-[60%] mx-auto mt-15 shadow-lg text-gray-600">
       <h1 className="text-2xl font-bold">Dashboard</h1>
       <p>
         Welcome back! Here you can manage your tasks, view analytics and make
         adjustments.
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 m-10 py-5">
-        <Link href="./tasks" className="bg-blue-100 rounded py-5 px-3">
+        <Link
+          href="./tasks"
+          className="bg-blue-200 rounded-xl py-5 px-3 opacity-80 hover:opacity-100 hover:scale-105"
+        >
           <div className="flex justify-between text-xl font-semibold items-center">
             <span>Tasks</span> <span className="text-2xl">📝</span>
           </div>
@@ -17,7 +20,10 @@ const Dashboard = () => {
             View, organize and manage your daily tasks.
           </p>
         </Link>
-        <Link href="./tasks" className="bg-green-100 rounded py-5 px-3">
+        <Link
+          href="./analytics"
+          className="bg-green-200 rounded-xl py-5 px-3 opacity-80 hover:opacity-100 hover:scale-105"
+        >
           <div className="flex justify-between text-xl font-semibold items-center">
             <span>Analytics</span> <span className="text-2xl">📊</span>
           </div>
@@ -25,7 +31,10 @@ const Dashboard = () => {
             Gain insights about your performance and trends.
           </p>
         </Link>
-        <Link href="./tasks" className="bg-pink-100 rounded py-5 px-3">
+        <Link
+          href="./settings"
+          className="bg-pink-200 rounded-xl py-5 px-3 opacity-80 hover:opacity-100 hover:scale-105"
+        >
           <div className="flex justify-between text-xl font-semibold items-center">
             <span>Settings</span> <span className="text-2xl">⚙️</span>
           </div>
@@ -33,7 +42,10 @@ const Dashboard = () => {
             Customize and tweek your dashboard settings.
           </p>
         </Link>
-        <Link href="./tasks" className="bg-yellow-100 rounded py-5 px-3">
+        <Link
+          href="./users"
+          className="bg-yellow-200 rounded-xl py-5 px-3 opacity-80 hover:opacity-100 hover:scale-105"
+        >
           <div className="flex justify-between text-xl font-semibold items-center">
             <span>Users</span> <span className="text-2xl">🧑‍🤝‍🧑</span>
           </div>
