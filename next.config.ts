9f1183913c2a6d2d3faf0f74b2756450e499d7a4
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-11-28 23:50:40 +0200

    GET method

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: true,
-  images: { domains: ["4kwallpapers.com"] },
 };
 
 export default nextConfig;
