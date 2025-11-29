9e407977fe11751f468365be45167b7c800293b6
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-11-29 21:46:01 +0200

    POST Method

--- app/movies/route.ts
+++ app/movies/route.ts
@@ -3,3 +3,9 @@ import { movies } from "./db";
 export async function GET() {
   return Response.json(movies);
 }
+
+export async function POST(req: Request) {
+  const movie = await req.json();
+  movies.push(movie);
+  return Response.json(movies);
+}
