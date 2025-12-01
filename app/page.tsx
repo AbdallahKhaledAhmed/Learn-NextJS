a9b179366a1460403e84571eb86d90081789789c
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-12-01 14:22:42 +0200

    Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Bebas_Neue } from "next/font/google";
 const bebasFont = Bebas_Neue({ subsets: ["latin"], weight: "400" });
 
 const Home = () => {
-  return <div className={`${bebasFont.className}`}>home page</div>;
+  return <div className={bebasFont.className}>home page</div>;
 };
 
 export default Home;
