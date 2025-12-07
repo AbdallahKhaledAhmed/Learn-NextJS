9452624d7a707688a744d47323888a268f0aa2f7
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-12-08 00:10:28 +0200

    Finished Layout Challenge

---
+++ app/blog/1/page.tsx
@@ -0,0 +1,15 @@
+import BlogLayout from "../layout";
+
+const Blog1 = () => {
+  return (
+    <BlogLayout
+      title="Blog Post 1
+"
+    >
+      <h3>Blog Post Title</h3>
+      <p>This is the content of the blog post.</p>
+    </BlogLayout>
+  );
+};
+
+export default Blog1;
--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,7 +1,24 @@
-const BlogLayout = () => {
+import Link from "next/link";
+import { ReactNode } from "react";
+
+const BlogLayout = ({
+  children,
+  title,
+}: {
+  children: ReactNode;
+  title?: string;
+}) => {
   return (
     <div>
-      <header></header>
+      <header className="p-6 bg-gray-900 text-3xl text-white">{title}</header>
+      <div className="flex p-3 gap-5">
+        <aside className="w-64 flex flex-col [&>a]:py-2 [&>a]:hover:bg-gray-300 ">
+          <Link href={"./1"}>Post1</Link>
+          <Link href={"./2"}>Post2</Link>
+          <Link href={"./3"}>Post3</Link>
+        </aside>
+        <main className="py-2">{children}</main>
+      </div>
     </div>
   );
 };
