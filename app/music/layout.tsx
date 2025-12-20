6a757cacae1fb7d6b51d21006fb98aa02eb78b2a
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-12-20 23:55:56 +0200

    finished Music Player Project

--- app/music/layout.tsx
+++ app/music/layout.tsx
@@ -1,5 +1,25 @@
-interface LayoutProps {}
+import { ReactNode } from "react";
 
-export function Layout(props: LayoutProps) {
-  return <div></div>;
+interface LayoutProps {
+  children: ReactNode;
+  sidebar: ReactNode;
+  songs: ReactNode;
+  player: ReactNode;
+}
+
+export default function Layout({
+  children,
+  sidebar,
+  songs,
+  player,
+}: LayoutProps) {
+  return (
+    <div className="bg-[#222222] size-full min-h-screen text-white flex">
+      {sidebar}
+      <div className="flex flex-col w-[97%] mx-auto py-5 px-10 justify-between">
+        {songs}
+        {player}
+      </div>
+    </div>
+  );
 }
