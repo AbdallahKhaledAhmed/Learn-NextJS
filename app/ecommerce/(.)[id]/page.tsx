738f70745b9b0a6753c6153867b428639a506006
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-12-21 20:28:31 +0200

    Intercepting Routes

---
+++ app/ecommerce/(.)[id]/page.tsx
@@ -0,0 +1,20 @@
+import { products } from "../page";
+
+interface InterceptProductProps {
+  params: { id: string };
+}
+
+export default async function InterceptProduct(props: InterceptProductProps) {
+  const { id } = await props.params;
+  const product = products.find((p) => p.id.toString() === id);
+
+  return (
+    <div className="bg-black min-h-screen center">
+      <div className="mx-auto p-10  flex-col bg-white gap-5 text-3xl center rounded-lg border border-gray-300">
+        <h2>{product?.name}</h2>
+        <img src={product?.image} alt={product?.name} className="w-160" />
+        <p>Price: {product?.price}</p>
+      </div>
+    </div>
+  );
+}
