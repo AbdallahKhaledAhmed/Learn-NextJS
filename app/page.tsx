980ec05aec709cb051e53b34af36fdf15538dc5a
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-12-29 23:58:31 +0200

    added link to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen center flex-col gap-5">
+      <h1>this is the home page</h1>
+      <Link
+        href="/form"
+        className="inline-flex items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+      >
+        Go to Form
+      </Link>
+    </div>
+  );
 }
