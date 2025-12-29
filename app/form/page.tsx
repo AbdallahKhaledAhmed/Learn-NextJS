13b94fbbfdc8367f90b112f9cd578ce057abbc6d
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-12-29 23:43:26 +0200

    Todo app with server form actions

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,24 +1,10 @@
-import { prisma } from "@/lib/prisma";
-
-async function addTodo(formData: FormData) {
-  "use server";
-  const task = formData.get("todo") as string;
-  const newTodo = await prisma.todo.create({ data: { task: task } });
-  console.log(newTodo);
-}
-
-async function getTodos() {
-  const tasks = await prisma.todo.findMany({
-    select: { id: true, task: true },
-  });
-  return tasks;
-}
+import * as actions from "@/actions/actions";
 
 export default async function Form() {
-  const tasks = await getTodos();
+  const tasks = await actions.getTodos();
   return (
     <div className="min-h-screen center flex-col gap-5 w-100 mx-auto">
-      <form action={addTodo} className="space-y-3 w-full">
+      <form action={actions.addTodo} className="space-y-3 w-full">
         <div>
           <div className="mt-2">
             <input
@@ -33,13 +19,32 @@ export default async function Form() {
           Submit
         </button>
       </form>
-      <div className="w-full">
+      <div className="w-full space-y-3">
         {tasks.map((task) => (
-          <div className="flex justify-between w-full">
-            <p>{task.task}</p>
-            <div className="flex">
-              <button>Edit</button>
-              <button>Delete</button>
+          <div
+            className="flex items-center justify-between w-full gap-3"
+            key={task.id}
+          >
+            <div className="center justify-between gap-4 w-full [&_input]:hidden">
+              <form className="contents" action={actions.editTodo}>
+                <input
+                  placeholder="Task"
+                  type="text"
+                  className="flex! h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                  name="todo-edit"
+                  defaultValue={task.task}
+                />
+                <input type="text" name="todo-id" value={task.id} />
+                <button className="inline-flex items-center justify-center rounded-md bg-gray-700 px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80">
+                  Edit
+                </button>
+                <button
+                  formAction={actions.removeTodo}
+                  className="inline-flex items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                >
+                  Delete
+                </button>
+              </form>
             </div>
           </div>
         ))}
