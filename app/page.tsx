d83806153980aa577b9971150a8cc986d7bed156
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2026-01-01 09:54:35 +0200

    Form Validation with zod and react hook form

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="min-h-screen center flex-col gap-10">
-      <h1 className="text-6xl font-bold text-gray-700">🎬 Movies App</h1>
+      <h1 className="text-6xl font-bold text-gray-700">Welcome to my App</h1>
       <div className="space-x-3">
         <Link
-          href="/add-movie"
+          href="/sign-up"
           className="p-3 text-white rounded-lg hover:opacity-75 bg-blue-600"
         >
-          Add Movie
-        </Link>
-        <Link
-          href="/movies"
-          className="p-3 text-white rounded-lg hover:opacity-75 bg-gray-600"
-        >
-          View all Movies
+          Sign Up
         </Link>
       </div>
     </div>
